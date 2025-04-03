f584ffa1dd8b05049cf5755ddd9eef21b3c88f1d
Enock Chisiwa <enock.chisiwa@student.moringaschool.com>
2025-04-03 17:04:57 +0300

    initiate

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = (parameter1, parameter2) => {
+    return 20
+}
+divide(2000, 100);
+
+
+//arrow function called square
+const square = x => {
+     return x * x
+}
+square(4);
+
+
+// arrow function called add
+const add = (a, b) => {
+   return a + b ;
+}
+add(3, 4);
